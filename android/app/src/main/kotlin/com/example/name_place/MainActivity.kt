46e7fce9c80e7661dd1d5ce52f2bc975e4cf26f7
Gurmeet Singh Khalsa <nonubitta@gmail.com>
2026-09-22 16:18:09 -0700

    feat: Update package and application identifiers to use the correct namespace

--- android/app/src/main/kotlin/com/example/name_place/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.name_place
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/org/keertanpothi/name_place/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.keertanpothi.name_place
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
